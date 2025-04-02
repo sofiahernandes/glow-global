444dc47abcb93949362c7dd68c477ae9619267ff
Sofia B. Hernandes <sofiahernandes.dev@gmail.com>
2025-04-02 14:13:01 -0300

    Imagens em português

--- lib/products.ts
+++ lib/products.ts
@@ -17,7 +17,7 @@ export const products = [
       "This comprehensive and intuitive tool makes managing and organizing your finances infinitely easier. With spreadsheets, analyses, charts, and overviews, you'll achieve the financial results you've always dreamed of! The My Finances planner includes an annual overview of your income and expenses, investments tracking, goal management, and detailed monthly spreadsheets where you can log your finances in different categories, types, and more.",
     quote:
       "'Reflect daily on your progress and your purpose—this inner dialogue is the compass that will guide your journey to success.'✨ – Warrant Buffet",
-    additionalImages: ["/phoneMyFinances.png"],
+    additionalImages: ["/phoneMinhasFinancas.png"],
     formats: ["PDF", "Google Sheets"],
     url: "https://gumroad.com/checkout?layout=profile&product=koudro&quantity=1&referrer=https%3A%2F%2Fglowmerch.gumroad.com%2F%3F_gl%3D1*yjant9*_ga*MTc5MjUyMTE2NC4xNzQyNDk2OTk1*_ga_6LJN6D94N6*MTc0MzQ0OTk2Ny4yMC4wLjE3NDM0NDk5NjcuMC4wLjA.&_gl=1*1gtwzd5*_ga*MTc5MjUyMTE2NC4xNzQyNDk2OTk1*_ga_6LJN6D94N6*MTc0MzQ0OTk2Ny4yMC4xLjE3NDM0NTAwODEuMC4wLjA."
   },
@@ -27,7 +27,7 @@ export const products = [
     slug: "my-studies",
     category: ["Planners"],
     price: 9.99,
-    image: "/ipadMyStudies.png",
+    image: "/ipadMeusEstudos.jpg",
     features: [
       "Active study progress analysis",
       "Organize study by subject, topic, and date",
@@ -38,7 +38,7 @@ export const products = [
       "This comprehensive and intuitive tool makes managing and organizing your studies infinitely easier. With spreadsheets, analyses, charts, and overviews, you'll achieve the academic results you've always dreamed of! The My Studies planner includes multiple-choice and essay questions analysis, study time and insights tracking, and detailed charts which track your mistakes.",
     quote:
       "'When it comes to the design of effective learning experiences, one provocative question is worth a hundred proclamations.'✨ – Bernard Bull",
-    additionalImages: ["/phoneMyStudies.png"],
+    additionalImages: ["/phoneMeusEstudos.png"],
     formats: ["PDF", "Google Sheets"],
     url: "https://gumroad.com/checkout?layout=profile&product=ytytfu&quantity=1&referrer=https%3A%2F%2Fglowmerch.gumroad.com%2F%3F_gl%3D1*yjant9*_ga*MTc5MjUyMTE2NC4xNzQyNDk2OTk1*_ga_6LJN6D94N6*MTc0MzQ0OTk2Ny4yMC4wLjE3NDM0NDk5NjcuMC4wLjA.&_gl=1*1cjj03h*_ga*MTc5MjUyMTE2NC4xNzQyNDk2OTk1*_ga_6LJN6D94N6*MTc0MzQ0OTk2Ny4yMC4xLjE3NDM0NTAxNDEuMC4wLjA."
   },
@@ -48,7 +48,7 @@ export const products = [
     slug: "ultimate-planner",
     category: ["Planners", "Calendars"],
     price: 7.77,
-    image: "/ipadUltimatePlanner.png",
+    image: "/ipadPlannerDefinitivo.jpg",
     features: [
       "Daily schedule with hourly time blocks",
       "Weekly schedule for important highlights",
@@ -59,7 +59,7 @@ export const products = [
     description:
       "The Ultimate 2025 Personal Planner is designed to help you organize your life with style and efficiency. This comprehensive planner includes daily, weekly, and monthly views to keep you on track with all your goals and commitments. With dedicated sections for habit tracking, goal setting, and reflection, this planner is your all-in-one tool for a more productive and balanced life.",
     quote: "'The key is not to prioritize what's on your schedule, but to schedule your priorities.'✨ – Stephen Covey",
-    additionalImages: ["/phoneUltimatePlanner.png"],
+    additionalImages: ["/phonePlannerDefinitivo.png"],
     formats: ["PDF", "Printable Format"],
     url: "https://glowmerch.gumroad.com/l/ultimate-planner"
   },

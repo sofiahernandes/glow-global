34bb36c237e4892ea2787ef3bcb19a49fa89692b
Sofia B. Hernandes <sofiahernandes.dev@gmail.com>
2025-04-19 18:09:25 -0300

    Product Updates

--- lib/products.ts
+++ lib/products.ts
@@ -5,7 +5,7 @@ export const products = [
     slug: "my-finances",
     category: ["Planners"],
     price: 11.11,
-    mainImage: "/ipadMinhasFinancas.jpg",
+    mainImage: "/ipadMinhasFinancas.png",
     features: [
       "Monthly budget and income tracking",
       "Automatic calculations and responsive charts",
@@ -17,7 +17,7 @@ export const products = [
       "This comprehensive and intuitive tool makes managing and organizing your finances infinitely easier. With spreadsheets, analyses, charts, and overviews, you'll achieve the financial results you've always dreamed of! The My Finances planner includes an annual overview of your income and expenses, investments tracking, goal management, and detailed monthly spreadsheets where you can log your finances in different categories, types, and more.",
     quote:
       "'Reflect daily on your progress and your purpose—this inner dialogue is the compass that will guide your journey to success.'✨ – Warrant Buffet",
-    images: ["/ipadMinhasFinancas.jpg", "/phoneMinhasFinancas.png"],
+    images: ["/ipadMinhasFinancas.png", "/phoneMinhasFinancas.png"],
     formats: ["Google Sheets", "PDF", "Printable Format"],
     url: "https://gumroad.com/checkout?layout=profile&product=koudro&quantity=1&referrer=https%3A%2F%2Fglowmerch.gumroad.com%2F%3F_gl%3D1*yjant9*_ga*MTc5MjUyMTE2NC4xNzQyNDk2OTk1*_ga_6LJN6D94N6*MTc0MzQ0OTk2Ny4yMC4wLjE3NDM0NDk5NjcuMC4wLjA.&_gl=1*1gtwzd5*_ga*MTc5MjUyMTE2NC4xNzQyNDk2OTk1*_ga_6LJN6D94N6*MTc0MzQ0OTk2Ny4yMC4xLjE3NDM0NTAwODEuMC4wLjA."
   },
@@ -27,7 +27,7 @@ export const products = [
     slug: "my-studies",
     category: ["Planners"],
     price: 11.11,
-    mainImage: "/ipadMeusEstudos.jpg",
+    mainImage: "/ipadMeusEstudos.png",
     features: [
       "Active study progress analysis",
       "Organize study by subject, topic, and date",
@@ -38,7 +38,7 @@ export const products = [
       "This all-in-one, easy-to-use tool simplifies the way you manage and organize your studies. With built-in spreadsheets, insights, visuals, and summaries, you'll achive the academic success you've always dreamed of! The My Studies planner includes multiple-choice and essay questions analysis, study time and insights tracking, and detailed charts which track your mistakes.",
     quote:
       "'When it comes to the design of effective learning experiences, one provocative question is worth a hundred proclamations.'✨ – Bernard Bull",
-    images: ["/ipadMeusEstudos.jpg", "/phoneMeusEstudos.png"],
+    images: ["/ipadMeusEstudos.png", "/phoneMeusEstudos.png"],
     formats: ["Google Sheets", "PDF", "Printable Format"],
     url: "https://gumroad.com/checkout?layout=profile&product=ytytfu&quantity=1&referrer=https%3A%2F%2Fglowmerch.gumroad.com%2F%3F_gl%3D1*yjant9*_ga*MTc5MjUyMTE2NC4xNzQyNDk2OTk1*_ga_6LJN6D94N6*MTc0MzQ0OTk2Ny4yMC4wLjE3NDM0NDk5NjcuMC4wLjA.&_gl=1*1cjj03h*_ga*MTc5MjUyMTE2NC4xNzQyNDk2OTk1*_ga_6LJN6D94N6*MTc0MzQ0OTk2Ny4yMC4xLjE3NDM0NTAxNDEuMC4wLjA."
   },
@@ -48,7 +48,7 @@ export const products = [
     slug: "ultimate-planner",
     category: ["Planners", "Calendars"],
     price: 9.99,
-    mainImage: "/ipadPlannerDefinitivo.jpg",
+    mainImage: "/ipadPlannerDefinitivo.png",
     features: [
       "Daily schedule with hourly time blocks",
       "Weekly schedule for important highlights",
@@ -59,7 +59,7 @@ export const products = [
     description:
       "This complete, user-friendly tool transforms the way you organize your routine and habits. With daily, weekly, and monthly planners, insights, habit trackers, and calendars, you’ll be on the right track to achieving the life you’ve always dreamed of!",
     quote: "'You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose.' ✨ – Dr. Seuss",
-    images: ["/ipadPlannerDefinitivo.jpg", "/phonePlannerDefinitivo.png"],
+    images: ["/ipadPlannerDefinitivo.png", "/phonePlannerDefinitivo.png"],
     formats: ["PDF", "Printable Format"],
     url: "https://gumroad.com/checkout?product=vfess&quantity=1&_gl=1*jt9yaq*_ga*MTc5MjUyMTE2NC4xNzQyNDk2OTk1*_ga_6LJN6D94N6*MTc0Mzg5NzY3OC4yNy4xLjE3NDM4OTc4OTcuMC4wLjA."
   },

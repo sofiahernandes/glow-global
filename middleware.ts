fb5f9d86f0382c6d4116a12180ee07660cf48263
Sofia B. Hernandes <sofiahernandes.dev@gmail.com>
2025-04-21 11:54:36 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-import { auth0Config } from "@/lib/auth0/config";
-
-const auth0Edge = initAuth0Edge(auth0Config);
-
-/*
-export default async function middleware(request: NextRequest) {
-  const response = NextResponse.next();
-  const session = await auth0Edge.getSession(request, response);
-
-  if (!session) {
-    // Redirect to login page if not logged in
-    const url = new URL("/api/auth/login", request.url);
-    return NextResponse.redirect(url);
-  }
-
-  return response;
-}
-*/
-
 export const config = {
   matcher:
     "/((?!api/auth|_next/static|_next/image|favicon.ico|public|png|images).*)",

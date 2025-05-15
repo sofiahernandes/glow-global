8abb4ea437cf37570656d9d1c1a596bc1ebceeeb
Sofia Botechia Hernandes <sofiahernandes.dev@gmail.com>
2025-05-15 20:38:42 -0300

    Product Updates

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <main className="flex-1">
+        <div className="h-8 w-full fixed pt-1 flex justify-center bg-blue-600"><p className="text-white font-bold">Exclusive May Sales!</p></div>
         {/* Hero Section */}
         <section className="w-full py-12 md:py-24 lg:py-32 bg-blue-50">
-          <div className="h-6 w-full bg-blue-700"><p>Exclusive May Sales!</p></div>
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
